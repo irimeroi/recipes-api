5cd8c9908458d71121d151b9d8b9cd38d11d4c46
Sal Gonzalez <imdone151@gmail.com>
2023-09-28 21:51:49 -0700

    changed HTML to a recipe website

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# soccer-api
+# Recipe-api
+
+//We've changed our code! Now we are making a website for food. First idea is making recipes with user input such as I only have cheese and bread in my fridge what should I make? Grilled Cheese
+
+//references:
+
+//API 1
+
+//API 2
+
+//CSS Styling
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
-fetch("https://v3.football.api-sports.io/fixtures/rounds?season=2019&league=61", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "v3.football.api-sports.io",
-		"x-rapidapi-key": "71225f895694489bad3c82b84425c843"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
+//recipes api: edamam
+//https://developer.edamam.com/edamam-docs-recipe-api
+
+//wine pairing api
+//https://spoonacular.com/food-api/docs#Wine-Pairing
+
+
+// fetch("https://api.spoonacular.com/food/wine/pairing&apiKey=79dd044172b342eab0fc7efd361ced06", {
+// 	"method": "GET",
+// 	})
+	
+// .then(response => {
+// 	return response.json()
+// }).then(data => {
+// 	console.log(data);
+// })
+// .catch(err => {
+// 	console.log(err);
+// });
+
+var apiURL = 'https://api.edamam.com/api/recipes/v2.'
 
